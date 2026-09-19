452195eae49c12dc785affafec5bdd7b4077fe63
hibawahab356 <ranihiba356@gmail.com>
2026-09-19 16:19:24 +0500

    not

--- app.js
+++ app.js
@@ -24,10 +24,56 @@ signupBtn && signupBtn.addEventListener("click",(event)=>{
 let formdetails = document.querySelector("#alldata")
 
 
-formdetails && formdetails.addEventListener("submit",(event)=>{
+formdetails && formdetails.addEventListener("submit",async(event)=>{
     event.preventDefault()
+try{
+
     const alldata = new FormData(formdetails)
+    
     const data = Object.fromEntries(alldata)
-    console.log(data);
+
+    const {email,password,fullname} = data
+    console.log(email,password,fullname);
+
+// sign up authentication start
+    const { data:signupdata, error } = await client.auth.signUp({
+        email,
+        password,
+      })
+
+
+console.log(signupdata);
+console.log(error);
+
+      // signup code from supabse end
+
+      const id = signupdata?.user?.id
+      console.log(id);
+
+
+      const { error:insertionerror } = await client
+  .from('recipe_data')
+  .insert({ fullname,
+    user_id : id })
+
+
+    console.log(insertionerror);
+
+
+    if(signupdata){
+        console.log(signupdata,fullname);
+    }
+    else{
+        console.log(error.message);
+    }
+
+    
+
+}
+
+
+catch(error){
+    console.log(error);
+}
    
 })
